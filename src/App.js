c15d21d5efdcdf8523b23468ddc7ab6c8616bd17
Arthur Charuel <charuel.arthur@gmail.com>
2022-11-10 13:57:33 +0100

    Add analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import React from "react";
 import UploadComponent from "./component/UploadComponent";
-
+import { inject } from '@vercel/analytics';
 
 function App() {
+    inject();
     return (
         <div style={{ textAlign: "center" }}>
             <h1>VRZEN READER</h1>
