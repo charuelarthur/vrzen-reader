9e1e66f9229c3fbd677575a6100de6c0b9734201
Arthur Charuel <charuel.arthur@gmail.com>
2024-01-16 18:44:49 +0100

    Add analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from "react";
 import UploadComponent from "./component/UploadComponent";
 import { inject } from '@vercel/analytics';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
     inject();
@@ -10,6 +11,7 @@ function App() {
             <h1>VRZEN READER</h1>
             <br />
             <UploadComponent />
+            <SpeedInsights />
         </div>
     );
 }
